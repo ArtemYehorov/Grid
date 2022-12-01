e3b65d6680c3688616797268b2f528adecb78191
Artem <102017036+ArtemYehorov@users.noreply.github.com>
2022-12-01 14:45:36 +0100

    Добавьте файлы проекта.

---
+++ Grid/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            button1.Background = Brushes.Black;
+            button2.Background = Brushes.Green;
+            button3.Background = Brushes.DarkBlue;
+            button4.Background = Brushes.Black;
+            button5.Background = Brushes.DeepSkyBlue;
+            button6.Background = Brushes.Blue;
+            button7.Background = Brushes.DarkOrange;
+            button8.Background = Brushes.Yellow;
+            button9.Background = Brushes.White;
+            button10.Background = Brushes.SteelBlue;
+            button11.Background = Brushes.Cyan;
+            button12.Background = Brushes.Gray;
+            button13.Background = Brushes.LightGreen;
+            button14.Background = Brushes.Red;
+            button15.Background = Brushes.LightGray;
+            button16.Background = Brushes.DarkGray;
+            button17.Background = Brushes.DarkBlue;
+            button18.Background = Brushes.Gray;
+            button19.Background = Brushes.DarkRed;
+            button20.Background = Brushes.HotPink;
+            button21.Background = Brushes.Gray;
+            button22.Background = Brushes.Violet;
+            button23.Background = Brushes.LightBlue;
+            button24.Background = Brushes.Gold;
+            button25.Background = Brushes.Yellow;
+
+            this.Background = Brushes.Orange;
+        }
+    }
+}
